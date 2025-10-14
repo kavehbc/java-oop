4b1fc75d9e3b8a280ce2338eb125610b53576ae2
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2025-10-14 16:46:42 -0400

    Add Hello and MyLibrary classes to demonstrate basic Java functionality

---
+++ S13_my_library/Hello.java
@@ -0,0 +1,13 @@
+package S13_my_library;
+import S13_my_library.code.com.*;
+import S05_user_inputs.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+            Terminal.clear();
+            System.out.println("Hello World!");
+            MyLibrary.printMessage();
+    }
+
+}
---
+++ S13_my_library/code/com/MyLibrary.java
@@ -0,0 +1,14 @@
+package S13_my_library.code.com;
+
+public class MyLibrary {
+    
+    public static void printMessage(){
+        System.out.println("This is a message from Library.");
+    }
+}
+
+// Other classes should be in their own java file <ClassName.java>
+// public class SecondLibrary{
+
+
+// }
